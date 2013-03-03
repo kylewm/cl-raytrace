9cd8e9c0b3b53d10b32372ba69d5eb3695dfa6cd
Kyle Mahan <kyle.mahan@gmail.com>
2013-03-03 04:09:43 -0800

    refactored lists into classes where appropriate

--- raytrace.lisp
+++ raytrace.lisp
@@ -9,7 +9,6 @@
 (defparameter *viewplane-width* 10)
 (defparameter *viewplane-height* 10)
 
-
 (defun curry (function &rest args)
     (lambda (&rest more-args)
       (apply function (append args more-args))))
@@ -31,17 +30,11 @@
       (list x y)
       (list x y z)))
 
-(defun point-x (pt)
-  (car pt))
+(defun point-x (pt) (car pt))
+(defun point-y (pt) (cadr pt))
+(defun point-z (pt) (caddr pt))
 
-(defun point-y (pt)
-  (cadr pt))
-
-(defun point-z (pt)
-  (caddr pt))
-
-(defun square (x)
-  (* x x))
+(defun square (x) (* x x))
 
 (defun sum-of-squares (pt)
   (reduce #'+ (mapcar #'square pt)))
@@ -53,116 +46,95 @@
   (let ((mag (magnitude pt)))
     (mapcar (lambda (x) (/ x mag)) pt)))
 
-(defun make-ray (origin dir)
-  (list origin (normalize dir)))
+(defclass ray ()
+  ((origin :initarg :origin
+	   :reader origin)
+   (direction :initarg :direction
+	      :reader direction)))
+
+(defmethod initialize-instance :after ((ray ray) &key)
+  (setf (slot-value ray 'direction)
+	(normalize (slot-value ray 'direction))))
+
+(defun ray-x (ray) (point-x (origin ray)))
+(defun ray-y (ray) (point-y (origin ray)))
+(defun ray-z (ray) (point-z (origin ray)))
+
+(defun ray-i (ray) (point-x (direction ray)))
+(defun ray-j (ray) (point-y (direction ray)))
+(defun ray-k (ray) (point-z (direction ray)))
 
 (defun make-ray-from-points (start end)
-  (make-ray start (mapcar #'- end start)))
+  (make-instance 'ray :origin start :direction (mapcar #'- end start)))
 
-(defun ray-origin (ray)
-  (car ray))
+(defclass scene-object () 
+  ((color
+    :initarg :color
+    :reader color)))
 
-(defun ray-x (ray)
-  (point-x (ray-origin ray)))
+(defclass sphere (scene-object)
+  ((center
+    :initarg :center
+    :reader center)
+   (radius
+    :initarg :radius
+    :reader radius)))
 
-(defun ray-y (ray)
-  (point-y (ray-origin ray)))
+(defclass ray-intersection ()
+  ((time
+    :initarg :intersect-time
+    :reader intersect-time)
+   (normal
+    :initarg :normal
+    :reader normal)
+   (intersect-object
+    :initarg :intersect-object
+    :reader intersect-object)))
 
-(defun ray-z (ray)
-  (point-z (ray-origin ray)))
+(defgeneric find-intersection-object (ray object))
 
-(defun ray-dir (ray)
-  (cadr ray))
-
-(defun ray-i (ray)
-  (point-x (ray-dir ray)))
-
-(defun ray-j (ray)
-  (point-y (ray-dir ray)))
-
-(defun ray-k (ray)
-  (point-z (ray-dir ray)))
-
-(defun object-type (obj)
-  (car obj))
-
-(defun object-color (obj)
-  (cadr obj))
-
-(defun make-sphere (color center radius)
-  (list 'sphere color center radius))
-
-(defun sphere-center (sphere)
-  (caddr sphere))
-
-(defun sphere-radius (sphere)
-  (cadddr sphere))
-
-(defun sphere-l (sphere)
-  (point-x (sphere-center sphere)))
-
-(defun sphere-m (sphere)
-  (point-y (sphere-center sphere)))
-
-(defun sphere-n (sphere)
-  (point-z (sphere-center sphere)))
-
-(defun sphere-p (obj)
-  (eq (car obj) 'sphere))
-
-(defun make-intersection (time normal obj)
-  (list time (normalize normal) obj))
-
-(defun intersection-t (inters)
-  (car inters))
-
-(defun intersection-normal (inters)
-  (cadr inters))
-
-(defun intersection-object (inters)
-  (caddr inters))
-
-(defun find-intersection-sphere (ray sphere)
-  (let* ((a (sum-of-squares (ray-dir ray)))
-	 (b (+ (* 2 (ray-i ray) (- (ray-x ray) (sphere-l sphere)))
-	       (* 2 (ray-j ray) (- (ray-y ray) (sphere-m sphere)))
-	       (* 2 (ray-k ray) (- (ray-z ray) (sphere-n sphere)))))
-	 (c (+ (sum-of-squares (sphere-center sphere))
-	       (sum-of-squares (ray-origin ray))
-	       (* -2 (sphere-l sphere) (ray-x ray))
-	       (* -2 (sphere-m sphere) (ray-y ray))
-	       (* -2 (sphere-n sphere) (ray-z ray))
-	       (- (square (sphere-radius sphere)))))
-	 (det (- (square b) (* 4 a c))))
-    
+(defmethod find-intersection-object (ray (object sphere))
+    (let* ((radius (radius object))
+	   (sphere-l (point-x (center object)))
+	   (sphere-m (point-y (center object)))
+	   (sphere-n (point-z (center object)))
+	   (a (sum-of-squares (direction ray)))
+	   (b (+ (* 2 (ray-i ray) (- (ray-x ray) sphere-l))
+		 (* 2 (ray-j ray) (- (ray-y ray) sphere-m))
+		 (* 2 (ray-k ray) (- (ray-z ray) sphere-n))))
+	   (c (+ (sum-of-squares (center object))
+		 (sum-of-squares (origin ray))
+		 (* -2 sphere-l (ray-x ray))
+		 (* -2 sphere-m (ray-y ray))
+		 (* -2 sphere-n (ray-z ray))
+		 (- (square radius))))
+	   (det (- (square b) (* 4 a c))))
+      
     (if (< det 0)
 	nil
 	(let* ((t1 (/ (+ (- b) (sqrt det)) (* 2 a)))
 	       (t2 (/ (- (- b) (sqrt det)) (* 2 a)))
 	       (time (min t1 t2))
-	       (normal (make-point (/ (- (ray-x ray) (sphere-l sphere)) (sphere-radius sphere))
-				   (/ (- (ray-y ray) (sphere-m sphere)) (sphere-radius sphere))
-				   (/ (- (ray-z ray) (sphere-n sphere)) (sphere-radius sphere)))))
-	  (make-intersection time normal sphere)))))
+	       (normal (make-point (/ (- (ray-x ray) sphere-l) radius)
+				   (/ (- (ray-y ray) sphere-m) radius)
+				   (/ (- (ray-z ray) sphere-n) radius))))
+	  (make-instance 'ray-intersection :intersect-time time :normal normal :intersect-object object)))))
 
-(defun find-intersection-object (ray obj)
-  (cond ((sphere-p obj) (find-intersection-sphere ray obj))
-	(t nil)))
+(defparameter *scene*
+  (list
+   (make-instance 'sphere :color (make-color 0 0 255) :center (make-point 20 5 -150) :radius 30)
+   (make-instance 'sphere :color (make-color 255 0 0) :center (make-point -10 -5 -100) :radius 30)))
 
 (defun find-intersection (ray)
   (reduce 
    (lambda (best-int obj) 
      (let ((obj-int (find-intersection-object ray obj)))
        (if (or (null best-int)
-	       (and obj-int (< (intersection-t obj-int) (intersection-t best-int))))
+	       (and obj-int (< (intersect-time obj-int) (intersect-time best-int))))
 	   obj-int
 	   best-int))) 
    *scene* :initial-value nil))
 
-(defparameter *scene*
-  (list
-   (make-sphere (make-color 0 0 255) (make-point 20 5 -150) 30)
-   (make-sphere (make-color 255 0 0) (make-point -10 -5 -100) 30)))
 
 (defun set-pixel (img pt color)
   (setf (aref img (point-y pt) (point-x pt) 0) (color-red color))
@@ -172,7 +144,7 @@
 (defun shoot-ray (ray)
   (let ((inters (find-intersection ray)))
     (if inters
-	(object-color (intersection-object inters))
+	(color (intersect-object inters))
 	(make-color 0 0 0))))
 
 (defun raytrace (output-pathname)
